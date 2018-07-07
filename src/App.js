a6d91eea6ad8845d5eefb0b38c7ee6938f8522a0
Stephanie Sunshine <ponyosunshine@gmail.com>
2018-07-07 13:19:40 -0700

    rearranged and started using state to modify the class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
-import { HexGrid, Layout, Hexagon, Path } from 'react-hexgrid';
-import logo from './logo.svg';
+import { HexGrid, Layout, Hexagon } from 'react-hexgrid'; // Path
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.hexagonSize = { x: 3, y: 3};
+    this.min = -6;
+    this.max = 6;
+    this.starting_grid = [];
+    this.state = {class: "defaultTile"};
+    var q;
+    var r;
+
+
+    for(r=this.min;r<=0;r++) {
+      for(q=this.min-r;q<=this.max;q++) {
+        this.starting_grid.push([q,r]);
+      }
+    }
+
+    for(r=1;r<=this.max;r++) {
+      for(q=this.min;q<=this.max-r;q++) {
+        this.starting_grid.push([q,r]);
+      }
+    }
+  }
+
   onClick(event) {
     console.log(event);
     console.log(this);
+    this.setState({class: 'hitTile'});
   }
 
   onMouseOver() {
@@ -18,35 +43,14 @@ class App extends Component {
   }
 
   render() {
-    const hexagonSize = { x: 3, y: 3};
-    const min = -6;
-    const max = 6;
-    var starting_grid = [];
-    var q;
-    var r;
-
-    for(r=min;r<=0;r++) {
-      for(q=min-r;q<=max;q++) {
-        starting_grid.push([q,r]);
-      }
-    }
-
-    for(r=1;r<=max;r++) {
-      for(q=min;q<=max-r;q++) {
-        starting_grid.push([q,r]);
-      }
-    }
-
-    console.log(starting_grid);
-
     return (
       <div className="App">
         <HexGrid width={1200} height={800}>
         {/* Grid with manually inserted hexagons */}
-          <Layout size={hexagonSize} flat={false} spacing={1.1} origin={{ x: 0, y: 0 }}>
-            {starting_grid.map((pair, index) => {
+          <Layout size={this.hexagonSize} flat={false} spacing={1.1} origin={{ x: 0, y: 0 }}>
+            {this.starting_grid.map((pair, index) => {
             var hexid="hexagon-"+index;
-            return(<Hexagon id={hexid} key={hexid} className="yellowShipTile" q={pair[0]} r={pair[1]} s={0} onClick={this.onClick.bind(this)} />);
+            return(<Hexagon id={hexid} key={hexid} className={this.state.class} q={pair[0]} r={pair[1]} s={0} onClick={this.onClick.bind(this)} />);
             })}
           </Layout>
         </HexGrid>
